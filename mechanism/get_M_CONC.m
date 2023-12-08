961bc7fd2f1a947dfbc5c124016bc1d68136c0da
mvcm-tools <81830321+mvcm-tools@users.noreply.github.com>
2023-12-08 12:27:59 -0500

    Delete mechanism directory

--- mechanism/get_M_CONC.m
+++
@@ -1,5 +0,0 @@
-function [ M_CONC ] = get_M_CONC(  )
-   global AIRConc
-   M_CONC = AIRConc;
-end
-
--- mechanism/get_xhet.m
+++
@@ -1,8 +0,0 @@
-function [ xxhet ] = get_xhet(  )
-%get value for heterogenous chemistry
-% 1 for heterogenous reactions on
-% 0 for heterogenous reactions off
-    global xhet;
-    xxhet = xhet;
-end
-
